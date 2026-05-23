a721bbf7283d205035584f219938cc6c228729c5
wordigabriel <wordigabriel5@gmail.com>
2026-05-23 20:34:07 +0000

    Add files via upload

---
+++ Backend/src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  const uri = process.env.MONGO_URI;
+  if (!uri) {
+    throw new Error("MONGO_URI is missing in environment variables");
+  }
+  await mongoose.connect(uri);
+  console.log("MongoDB connected");
+};
+
+module.exports = connectDB;
